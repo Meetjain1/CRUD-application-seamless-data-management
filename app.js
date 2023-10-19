9ea8f85d6d4fca0758c365e85bdbee55f8fa8985
Meetjain1 <133582566+Meetjain1@users.noreply.github.com>
2023-10-19 13:30:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const books = require('./books'); 
+
+app.use(bodyParser.json());
+
+
+app.get('/books', (req, res) => {
+  res.json(books.getAll());
+});
+
+app.get('/books/:id', (req, res) => {
+  const book = books.getById(req.params.id);
+  if (book) {
+    res.json(book);
+  } else {
+    res.status(404).json({ error: 'Book not found' });
+  }
+});
+
+
+app.post('/books', (req, res) => {
+  const book = req.body;
+  const newBook = books.create(book);
+  res.status(201).json(newBook);
+});
+
+
+app.put('/books/:id', (req, res) => {
+  const updatedBook = books.update(req.params.id, req.body);
+  if (updatedBook) {
+    res.json(updatedBook);
+  } else {
+    res.status(404).json({ error: 'Book not found' });
+  }
+});
+
+
+app.delete('/books/:id', (req, res) => {
+  const deleted = books.delete(req.params.id);
+  if (deleted) {
+    res.status(204).end();
+  } else {
+    res.status(404).json({ error: 'Book not found' });
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ book.js
@@ -0,0 +1,45 @@
+const books = [
+    { id: '1', title: 'Book 1', author: 'Author 1' },
+    { id: '2', title: 'Book 2', author: 'Author 2' },
+  ];
+  
+  function getAll() {
+    return books;
+  }
+  
+  function getById(id) {
+    return books.find(book => book.id === id);
+  }
+  
+  function create(book) {
+    const newBook = { id: (books.length + 1).toString(), ...book };
+    books.push(newBook);
+    return newBook;
+  }
+  
+  function update(id, bookData) {
+    const index = books.findIndex(book => book.id === id);
+    if (index !== -1) {
+      books[index] = { ...books[index], ...bookData };
+      return books[index];
+    }
+    return null;
+  }
+  
+  function deleteBook(id) {
+    const index = books.findIndex(book => book.id === id);
+    if (index !== -1) {
+      books.splice(index, 1);
+      return true;
+    }
+    return false;
+  }
+  
+  module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    delete: deleteBook,
+  };
+  
